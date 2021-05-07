0fdca6314653ebb07b56cab4e6d10e2517e1b121
galyakir <gal.yakir@gmail.com>
2021-05-07 17:55:09 +0300

    Update vars.yml

--- staging/output.tf
+++ staging/output.tf
@@ -5,15 +5,14 @@ output "staging_url" {
 resource "local_file" "prod_output" {
   content = <<-EOT
     ---
-
-    hosts_name: var.tag_name
-    host_url: module.lb.app_lb.dns_name
-    okta_url: var.okta_url
-    okta_id: var.okta_id
-    okta_secret: var.okta_secret
-    okta_key: var.okta_key
-    okta_label: var.okta_label
-    db_host: aws_db_instance.app_DB.address
+    hosts_name: ${var.tag_name}
+    host_url: ${module.lb.app_lb.dns_name}
+    okta_url: ${var.okta_url}
+    okta_id: ${var.okta_id}
+    okta_secret: ${var.okta_secret}
+    okta_key: ${var.okta_key}
+    okta_label: ${var.okta_label}
+    db_host: ${aws_db_instance.app_DB.address}
    EOT
   filename = "../vars.yml"
 }
